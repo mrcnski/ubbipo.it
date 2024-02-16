1c0a9fd8702d510891b4856a713e7022ef1c29f7
Marcin S. <marcin@realemail.net>
2024-02-16 18:58:39 +0100

    Add drinks

---
+++ oreste/drinks.js
@@ -0,0 +1,65 @@
+export const beers = [
+    {
+        name: "Unachiara",
+        description: "Helles, 33 cl. vol. 5.2%",
+        producer: "Birrificio Fieij",
+        location: "Castelnuovo Cilento",
+        price: "5",
+    },
+    {
+        name: "Unafresca",
+        description: "Hefe-Weizen, 33 cl. vol. 5.5%",
+        producer: "Birrificio Fieij",
+        location: "Castelnuovo Cilento",
+        price: "5",
+    },
+    {
+        name: "Unambrata",
+        description: "American Pale Ale, 33 cl. vol. 6%",
+        producer: "Birrificio Fieij",
+        location: "Castelnuovo Cilento",
+        price: "5",
+    },
+    {
+        name: "Unaspecial",
+        description: "Belgian Dark Ale, 33 cl. vol. 6%",
+        producer: "Birrificio Fieij",
+        location: "Castelnuovo Cilento",
+        price: "5",
+    },
+    {
+        name: "Unaforte",
+        description: "Belgian Strong Ale, 33 cl. vol. 8.5%",
+        producer: "Birrificio Fieij",
+        location: "Castelnuovo Cilento",
+        price: "5",
+    },
+];
+
+export const drinks = [
+    {
+        name: "Acqua Microfiltrata",
+        description: "Sistema Culligan, 75 cl",
+        price: "2",
+    },
+    {
+        name: "Cedrata Tassoni",
+        price: "3",
+    },
+    {
+        name: "Mirtonic",
+        price: "7",
+    },
+    {
+        name: "Spritz",
+        price: "7",
+    },
+    {
+        name: "Artisan liqueurs",
+        price: "4",
+    },
+    {
+        name: "Amaro Di Sassano Penna",
+        price: "3",
+    },
+];
