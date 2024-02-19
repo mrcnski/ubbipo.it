c89c27657e018d49a6ae47f97f54fa70ed0eec4f
Marcin S. <marcin@realemail.net>
2024-02-19 11:26:08 +0100

    Add page descriptions

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -11,9 +11,13 @@ export const ui = {
         'nav.wine': 'Wine',
         'nav.drinks': 'Drinks',
         'nav.languages': 'Languages',
+
+        'index.description': "Ubbìpò Osteria is a family restaurant in Cilento. We provide homemade, traditional Italian food for a unique culinary and cultural experience.",
         
         'menu.wine-menu': 'Wine Menu',
+        'menu.wine-description': 'Our wine list is carefully selected to offer a wide range of wines from different regions of Italy. We have a variety of red, white, rosé, lively, and sparkling wines. We also offer vegan and no-sulfites-added wines.',
         'menu.drinks-menu': 'Beer and Drinks Menu',
+        'menu.drinks-description': 'Our beer and drinks menu offers a variety of beers and non-alcoholic drinks.',
         
         'menu.vini-rossi': 'Red',
         'menu.vini-bianchi': 'White',
@@ -35,9 +39,13 @@ export const ui = {
         'nav.wine': 'Vini',
         'nav.drinks': 'Bevande',
         'nav.languages': 'Lingue',
+
+        'index.description': "Ubbìpò Osteria è un ristorante a conduzione familiare nel Cilento. Forniamo cibo italiano tradizionale fatto in casa per un'esperienza culinaria e culturale unica.",
         
         'menu.wine-menu': 'Carta dei Vini',
+        'menu.wine-description': "La nostra carta dei vini è accuratamente selezionata per offrire una vasta gamma di vini provenienti da diverse regioni d'Italia. Disponiamo di una varietà di vini rossi, bianchi, rosati, vivaci e spumanti. Offriamo anche vini vegani e senza solfiti aggiunti.",
         'menu.drinks-menu': 'Menù Birre e Bevande',
+        'menu.drinks-description': 'Il nostro menu di birre e bevande offre una varietà di birre e bevande analcoliche.',
         
         'menu.vini-rossi': 'Rossi',
         'menu.vini-bianchi': 'Bianchi',
