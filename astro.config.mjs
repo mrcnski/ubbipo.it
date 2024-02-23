c9d7a8ea5fc41777b8844861f1fb624e58721785
Marcin S. <marcin@realemail.net>
2024-02-23 11:58:12 +0100

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-    i18n: {
-        defaultLocale: "it",
-        locales: ["en", "it"],
-        routing: {
-            prefixDefaultLocale: false
-        }
+  i18n: {
+    defaultLocale: "it",
+    locales: ["en", "it"],
+    routing: {
+      prefixDefaultLocale: false
     }
+  },
+  integrations: [sitemap()]
 });
