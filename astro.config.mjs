e98a472e02ab5436e7453a14b02328a46dc2c813
Marcin S. <marcin@realemail.net>
2024-02-23 20:55:34 +0100

    Try adding OpenGraph meta tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ubbipo.it',
   i18n: {
     defaultLocale: "it",
     locales: ["en", "it"],
