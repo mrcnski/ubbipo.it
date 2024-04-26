845f34a9a1b8e8dc67c57e9707924bd1ccb9b209
agenoriasnc <160594104+agenoriasnc@users.noreply.github.com>
2024-04-26 16:40:43 +0200

    Update food.js

--- oreste/food.js
+++ oreste/food.js
@@ -195,7 +195,7 @@ export const dishes = [
         image: "Cavatelli.jpeg",
         producer: "",
         location: "",
-        tags: "vegetarian", "vegan",
+        tags: "vegetarian vegan",
         price: {
             it: '*10*',
             en: '*10*',
