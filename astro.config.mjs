f7bfa8d5738a7df6304548212b9099773a5364ef
Marcin S. <marcin@realemail.net>
2024-05-13 13:12:23 -0400

    Fix site value

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ubbipo.it',
+  site: 'https://www.ubbipo.it',
   // Using view transitions enables prefetching for all links on the page.
   // prefetch: true,
   i18n: {
